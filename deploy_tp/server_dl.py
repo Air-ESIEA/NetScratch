9fedda343e5f067e6e80937dba8390abbdab7d39
mogwai <vmichel@intechinfo.fr>
2019-11-10 18:00:08 +0100

    added more text, css style, intech logo

--- deploy_tp/server_dl.py
+++ deploy_tp/server_dl.py
@@ -6,6 +6,26 @@ import urllib
 import re
 import threading
 
+# ----- Stylesheet -------
+
+style = '''
+<style type="text/css">
+body {
+    background-color: DimGrey;
+}
+input {
+	max-width: 500px;
+	padding: 10px 20px;
+	background: #f4f7f8;
+	margin: 10px auto;
+	padding: 20px;
+	background: #f4f7f8;
+	border-radius: 8px;
+	font-family: 'Arial';
+}
+
+</style>'''
+
 # ----- Fonctions --------
 
 def download_deploy(url_to_get:str, dirname:str):
@@ -57,10 +77,23 @@ def get_info():
         thread1 = threading.Thread(target=download_deploy, args=(url, dirname))
         thread1.start()        
         return 'Telechargement lancer'
-    return '''<form method = 'POST'>
-                <input type=text name=tp_name placeholder='Nom du TP'><br>
-                <input type=text name=url placeholder='URL du fichier à télécharger'><br>
-                <input type=submit value=Lancer>'''
+    return '''<!DOCTYPE html>
+                <head>
+                   '''+style+''' 
+                </head>
+                <font face='arial'>
+                <body>
+                <div align=center>
+                    <form method = 'POST'>
+                    <h1>Pré-télécharger les logiciels de TP :</h1><br>
+                        Nom du TP :<br><input type=text name=tp_name placeholder='Nom du TP'><br>
+                        URL : <br><input type=url name=url placeholder='URL du fichier à télécharger' required><br>
+                        <input type=submit value=Lancer>
+                    </form>
+                    <img src='https://www.intechinfo.fr/wp-content/uploads/2019/09/logo-intechinfo-sans-baseline.png'>
+                </div>
+                </font>
+                </body>'''
 
 #@app.route
 if __name__ == '__main__':
